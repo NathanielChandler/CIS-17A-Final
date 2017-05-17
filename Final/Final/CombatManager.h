b4c7db120a05b9581db4d720cc130ac9548f736d
Nathaniel <chandlerocks4@gmail.com>
2017-05-16 22:26:30 -0700

    CombatManager progress 2
    Removed local files, code is not ready, will hopefully have this done soon

--- Final/Final/CombatManager.h
+++ Final/Final/CombatManager.h
@@ -5,9 +5,12 @@
 class CombatManager
 {
 private:
+	shared_ptr<Player> _player;
+	shared_ptr<Enemy> _enemy;
 	string _enemyMoveset;
 	void getEnemyMoveset();
 	string enemyMoveManager();
+	string ruleBook(shared_ptr<Combatant> attacker,char attack, shared_ptr<Combatant> defender,char lastmove);
 public:
 	CombatManager(shared_ptr<Player> player, shared_ptr<Enemy> enemy);
 	~CombatManager();
