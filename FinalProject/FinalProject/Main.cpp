ce2c94be16c7d201eb11d0575de1b542bca7404d
Nathaniel <chandlerocks4@gmail.com>
2017-06-04 20:34:31 -0700

    Progress 1

--- FinalProject/FinalProject/Main.cpp
+++ FinalProject/FinalProject/Main.cpp
@@ -11,7 +11,8 @@ using namespace std;
 void startGame();
 void turn(shared_ptr<Player> player);
 void combat(shared_ptr<Player> player, shared_ptr<Combatant> enemy);
-void move();
+void move(shared_ptr<Player> player);
+void levelUp(shared_ptr<Player> player);
 /*create methods for:
 combat
 room navigation
@@ -106,14 +107,14 @@ void startGame()
 void turn(shared_ptr<Player> player)
 {
 	//ask to fight
-	if (dungeon.getCurrentLocation()->getOccupant()->isDead() == false)
+	if (dungeon.getCurrentLocation()->getOccupant() != nullptr && dungeon.getCurrentLocation()->getOccupant()->isDead() == false)
 	{
 		combat(player, dungeon.getCurrentLocation()->getOccupant());
 	}
-	//ask to move
+	//ask to do stuff
 	if (player->isDead() == false)
 	{
-		move();
+		move(player);
 	}
 }
 
@@ -160,17 +161,18 @@ void combat(shared_ptr<Player> player,shared_ptr<Combatant> enemy)
 	}
 }
 
-void move()
+void move(shared_ptr<Player> player)
 {
 	char in;
 	system("cls");
 	cout << dungeon.getCurrentLocation()->GenerateDescription() << endl;
-	cout << "Where would you like to go?\n\t[ ";
-	if (dungeon.getCurrentLocation()->getNorth()) cout << "N = North ";
-	if (dungeon.getCurrentLocation()->getSouth()) cout << "S = South ";
-	if (dungeon.getCurrentLocation()->getEast()) cout << "E = East ";
-	if (dungeon.getCurrentLocation()->getWest()) cout << "W = West ";
-	cout << " ]\n>>";
+	cout << "What would you like to do?\n";
+	if (dungeon.getCurrentLocation()->getNorth()) cout << "\tN = Go North \n";
+	if (dungeon.getCurrentLocation()->getSouth()) cout << "\tS = Go South \n";
+	if (dungeon.getCurrentLocation()->getEast()) cout << "\tE = Go East \n";
+	if (dungeon.getCurrentLocation()->getWest()) cout << "\tW = Go West \n";
+	if (dungeon.getCurrentLocation()->getAltar()) cout << "\tA = Use Altar \n";
+	cout << "\n>>";
 	cin >> in;
 	switch (toupper(in))
 	{
@@ -178,6 +180,66 @@ void move()
 	case 'S':dungeon.move(SOUTH); break;
 	case 'E':dungeon.move(EAST); break;
 	case 'W':dungeon.move(WEST); break;
+	case 'A':if (dungeon.getCurrentLocation()->getAltar()) levelUp(player); break;
 	default: cout << "That's not an option, you doltz";
+	}
+}
+
+void levelUp(shared_ptr<Player> player)
+{
+	//ressurect enemies(Not Bosses)
+	bool done = false;
+	while (!done)
+	{
+
+		system("cls");
+		int atkCost = player->getAtk() * 5;
+		int defCost = player->getDef() * 5;
+		int vitCost = player->getMaxVit() * 5;
+		int stmCost = player->getMaxStm() * 5;
+		//ask to level up
+		cout << "-Stat-\t\t-Power-\t\t-Cost-" << endl;
+		cout << " Atk \t\t" << player->getAtk() << "\t\t" << atkCost << " AuD\n"
+			<< " Def \t\t" << player->getDef() << "\t\t" << defCost << " AuD\n"
+			<< " Vit \t\t" << player->getMaxVit() << "\t\t" << vitCost << " AmD\n"
+			<< " Stm \t\t" << player->getMaxStm() << "\t\t" << stmCost << " AmD\n"
+			<< endl;
+		cout << "Current Dregs:\n\t" << player->getAud() << " AuD\n\t" << player->getAmd() << " AmD" << endl;
+		//system("pause");
+		char in;
+		cout << "What would you like to do?\n";
+		if (player->getAud() >= atkCost) cout << "\tA = Level up Attack\n";
+		if (player->getAud() >= defCost) cout << "\tD = Level up Defense\n";
+		if (player->getAmd() >= vitCost) cout << "\tV = Level up Vitality\n";
+		if (player->getAmd() >= stmCost) cout << "\tS = Level up Stamina\n";
+		cout << "\n>>";
+		cin >> in;
+		switch (toupper(in))
+		{
+		case 'A': 
+			if (player->getAud() >= atkCost)
+			{
+				player->setAud(player->getAud() - atkCost);
+				player->setAtk(player->getAtk() + 1);
+			}
+		case 'D':
+			if (player->getAud() >= atkCost)
+			{
+				player->setAud(player->getAud() - atkCost);
+				player->setAtk(player->getAtk() + 1);
+			}
+		case 'V':
+			if (player->getAud() >= atkCost)
+			{
+				player->setAud(player->getAud() - atkCost);
+				player->setAtk(player->getAtk() + 1);
+			}
+		case 'S':
+			if (player->getAud() >= atkCost)
+			{
+				player->setAud(player->getAud() - atkCost);
+				player->setAtk(player->getAtk() + 1);
+			}
+		}
 	}
 }
